b1b5b53057b678f0521d47dd6faf1d8212e34633
Gumraze-git <galaxydh4110@gmail.com>
2025-10-12 02:11:17 +0900

    feat(commitlint): 커밋 메시지 규칙에 제목 소문자 규칙 추가 (#1)
    - 제목(Subject)에 대문자 사용 금지 규칙 추가
    - `scope-case`, `subject-case` 검증 로직 설정
    - 커밋lint 구성 업데이트

--- commitlint.config.js
+++ commitlint.config.js
@@ -12,6 +12,9 @@ module.exports = {
   rules: {
     'header-max-length': [2, 'always', 72],
     'type-case': [2, 'always', 'lower-case'],
+    'scope-case': [2, 'always', 'lower-case'],
+    // 제목(Subject)에 대문자 사용 금지
+    'subject-case': [2, 'always', ['lower-case']],
     'subject-max-length': [2, 'always', 50],
     'subject-full-stop': [2, 'never', '.'],
     // 허용 타입을 명시(프로젝트 사용 패턴 반영)
