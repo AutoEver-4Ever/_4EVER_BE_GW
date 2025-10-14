808531b0320ac26ffe03ade833a2444a356de891
DonjJin-github <cdj7235@naver.com>
2025-10-14 19:52:29 +0900

    feat(pp): BOM 상세,목록 조회 dto 추가 (#3)

---
+++ src/main/java/org/ever/_4ever_be_gw/scmpp/dto/BomListItemDto.java
@@ -0,0 +1,23 @@
+package org.ever._4ever_be_gw.scmpp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BomListItemDto {
+    private Long bomId;
+    private String bomCode;
+    private Long productId;
+    private String productCode;
+    private String productName;
+    private String version;
+    private String status;
+    private LocalDateTime lastModifiedAt;
+}
