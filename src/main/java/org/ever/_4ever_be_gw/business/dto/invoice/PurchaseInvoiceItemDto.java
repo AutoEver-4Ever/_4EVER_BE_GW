bf8e42b5f560511aaf60078e121044e95d8767da
Gumraze-git <galaxydh4110@gmail.com>
2025-10-20 16:26:49 +0900

    refac(dto): 전표 dto 구조 및 필드 리팩토링 (#44)
    - 매입/매출 전표 DTO 통합 (`ApInvoiceDetailDto` → `InvoiceDetailDto`)
    - 미사용 매출 전표 DTO (`AsInvoiceDetailDto`) 삭제
    - 품목 관련 필드 데이터 타입 수정 (`Integer`, `Long`으로 변경)
    - 참조 정보 필드 단순화 및 불필요한 하위 클래스 제거
    - 통합된 전표 DTO에 전표 타입 필드 (`invoiceType`) 추가

--- src/main/java/org/ever/_4ever_be_gw/business/dto/invoice/PurchaseInvoiceItemDto.java
+++ src/main/java/org/ever/_4ever_be_gw/business/dto/invoice/PurchaseInvoiceItemDto.java
@@ -10,11 +10,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PurchaseInvoiceItemDto {
-    private Long itemId;        // 품목 ID
     private String itemName;    // 품목명
-    private int quantity;       // 수량
+    private Integer quantity;       // 수량
     private String uomName;     // 단위
-    private long unitPrice;     // 단가
-    private long totalPrice;    // 금액(수량*단가)
+    private Long unitPrice;     // 단가
+    private Long totalPrice;    // 금액(수량*단가)
 }
 
