3a3f80a6ebcb3a5cfdfdbb1da5cfb66b268775aa
Gumraze-git <galaxydh4110@gmail.com>
2025-10-27 19:25:36 +0900

    feat(service): hrm 인터페이스 및 기본 구현체 추가 (#71)
    - `HrmService` 인터페이스 생성 및 `createInternalUser` 메서드 정의
    - `HrmServiceImpl` 클래스 생성 (현재 빈 구현체 상태)

---
+++ src/main/java/org/ever/_4ever_be_gw/business/service/HrmService.java
@@ -0,0 +1,5 @@
+package org.ever._4ever_be_gw.business.service;
+
+public interface HrmService {
+    void createInternalUser();
+}
---
+++ src/main/java/org/ever/_4ever_be_gw/business/service/impl/HrmServiceImpl.java
@@ -0,0 +1,4 @@
+package org.ever._4ever_be_gw.business.service.impl;
+
+public class HrmServiceImpl {
+}
