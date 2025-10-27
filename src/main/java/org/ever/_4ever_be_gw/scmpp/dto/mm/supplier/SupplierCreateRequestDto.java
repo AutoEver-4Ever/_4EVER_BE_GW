a1acaceeefd166fd20da5249ce4e7fbdaec17875
gumraze-git <galaxydh4110@gmail.com>
2025-10-28 00:26:01 +0900

    chore(mm): dto 클래스 이름 변경 (#71)

--- src/main/java/org/ever/_4ever_be_gw/scmpp/dto/mm/supplier/SupplierCreateRequestDto.java
+++ src/main/java/org/ever/_4ever_be_gw/scmpp/dto/mm/supplier/SupplierCreateRequestDto.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 import org.ever._4ever_be_gw.business.dto.order.ManagerDto;
 
 @Getter
-public class MmSupplierCreateRequestDto {
+public class SupplierCreateRequestDto {
     private SupplierInfoDto supplierInfo;      // 공급업체 정보
     private ManagerDto managerInfo;        // 담당자 정보
     private List<MaterialItemsDto> materialList;
