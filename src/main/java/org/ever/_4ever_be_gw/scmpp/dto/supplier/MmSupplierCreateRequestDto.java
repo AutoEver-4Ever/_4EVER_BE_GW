8e7808c119f4ec2dfd5c9dad6dfc2f356149fd32
gumraze-git <galaxydh4110@gmail.com>
2025-10-28 00:17:03 +0900

    refac(dto): 공급업체 및 자재 관련 dto 분리 및 리팩토링 (#71)
    - `MmSupplierCreateRequestDto` 내 내장형 클래스 `SupplierInfo`와 `MaterialItems` 분리
    - `SupplierInfoDto` 및 `MaterialItemsDto` 클래스로 분리하여 모듈화 및 재사용성 강화
    - 관련 필드 타입 변경 및 import 추가

--- src/main/java/org/ever/_4ever_be_gw/scmpp/dto/supplier/MmSupplierCreateRequestDto.java
+++ src/main/java/org/ever/_4ever_be_gw/scmpp/dto/supplier/MmSupplierCreateRequestDto.java
@@ -3,28 +3,12 @@ package org.ever._4ever_be_gw.scmpp.dto.supplier;
 import java.util.List;
 import lombok.Getter;
 import org.ever._4ever_be_gw.business.dto.order.ManagerDto;
+import org.ever._4ever_be_gw.scmpp.dto.mm.MaterialItemsDto;
+import org.ever._4ever_be_gw.scmpp.dto.mm.SupplierInfoDto;
 
 @Getter
 public class MmSupplierCreateRequestDto {
-    private SupplierInfo supplierInfo;      // 공급업체 정보
+    private SupplierInfoDto supplierInfo;      // 공급업체 정보
     private ManagerDto managerInfo;        // 담당자 정보
-    private List<MaterialItems> materialList;
-
-    @Getter
-    public static class SupplierInfo {
-        private String supplierName;            // 공급업체 이름
-        private String supplierEmail;           // 공급업체 이메일
-        private String supplierPhone;           // 공급업체 전화번호
-        private String supplierBaseAddress;     // 공급업체 기본 주소
-        private String supplierDetailAddress;   // 공급업체 상세 주소
-        private String category;                // 제공하는 자재의 카테고리
-        private Integer deliveryLeadTime;       // 배송 기간
-    }
-
-    @Getter
-    public static class MaterialItems {     // 제공 자재 목록
-        private String materialName;        // 자재 이름
-        private String uomCode;             // 자재 단위 코드
-        private Integer unitPrice;          // 자재의 단위 가격
-    }
+    private List<MaterialItemsDto> materialList;
 }
