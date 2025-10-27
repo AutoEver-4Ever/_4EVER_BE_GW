295bb2e8706f9300c35c585e5ad403633327cfaa
gumraze-git <galaxydh4110@gmail.com>
2025-10-27 22:28:47 +0900

    feat(hrm): 내부 사용자 메서드 생성 (#71)
    - `Mono<EmployeeCreateRequestDto>`으로 내부 사용자 생성 구현체 추가
    - 메서드에 `EmployeeCreateRequestDto` 매개변수 추가
    - 비동기 처리 방식 적용을 위한 `Mono` 리액티브 타입 활용

--- src/main/java/org/ever/_4ever_be_gw/business/service/HrmService.java
+++ src/main/java/org/ever/_4ever_be_gw/business/service/HrmService.java
@@ -1,5 +1,10 @@
 package org.ever._4ever_be_gw.business.service;
 
+import org.ever._4ever_be_gw.business.dto.employee.EmployeeCreateRequestDto;
+import reactor.core.publisher.Mono;
+
 public interface HrmService {
-    void createInternalUser();
+    // 내부 사용자 등록
+    // Mono: 비동기 작업 결과가 있거나 없을 수 있음.
+    Mono<EmployeeCreateRequestDto> createInternalUser(EmployeeCreateRequestDto requestDto);
 }
