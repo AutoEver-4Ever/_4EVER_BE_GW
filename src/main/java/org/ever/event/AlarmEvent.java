0081a1230d7723edc9af28c8a7cbc2a3d05b2b10
parkjiwon <ji11won@naver.com>
2025-10-29 04:13:41 +0900

    feat(kafka): 알림 관련 토픽 데이터 형식 추가 (#89)

--- src/main/java/org/ever/event/AlarmEvent.java
+++ src/main/java/org/ever/event/AlarmEvent.java
@@ -1,9 +1,13 @@
 package org.ever.event;
 
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.ever.event.alarm.AlarmType;
+import org.ever.event.alarm.LinkType;
+import org.ever.event.alarm.TargetType;
 
 @Getter
 @SuperBuilder
@@ -11,24 +15,13 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 public class AlarmEvent extends BaseEvent {
 
-    private String userId;
-    private String title;
-    private String message;
-    private AlarmType alarmType;
-    private AlarmPriority priority;
-
-    public enum AlarmType {
-        PAYMENT_SUCCESS,
-        PAYMENT_FAILED,
-        ORDER_CONFIRMED,
-        SHIPMENT_NOTIFICATION,
-        GENERAL_NOTIFICATION
-    }
-
-    public enum AlarmPriority {
-        LOW,
-        MEDIUM,
-        HIGH,
-        URGENT
-    }
+    String alarmId;
+    AlarmType alarmType; // source와 구분하기 위해 alarm 명명
+    String targetId;
+    TargetType targetType;
+    String title;
+    String message;
+    String linkId;
+    LinkType linkType;
+    LocalDateTime scheduledAt; // 예약 발송 시간(아직 미구현) -> null 이면 즉시 발송
 }
