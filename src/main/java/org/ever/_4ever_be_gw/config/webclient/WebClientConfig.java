e9a6052ce5faafc68b16b63497b80fece37e7a9e
gumraze-git <galaxydh4110@gmail.com>
2025-10-31 11:22:10 +0900

    feat(webclient): web client 설정 및 타임아웃 설정 개선 (#102)
    - 기본 WebClient 설정에 연결 및 응답 타임아웃 추가
    - 장시간 타임아웃 처리를 위한 추가 WebClient 설정 메서드 제공
    - 공통 HttpClient 및 헤더 설정 적용

--- src/main/java/org/ever/_4ever_be_gw/config/webclient/WebClientConfig.java
+++ src/main/java/org/ever/_4ever_be_gw/config/webclient/WebClientConfig.java
@@ -18,6 +18,12 @@ import reactor.netty.http.client.HttpClient;
 @EnableConfigurationProperties(ApiProperties.class)
 public class WebClientConfig {
 
+    // WebClient 설정
+    // 연결 시도 시간: 서버에 연결하는데 대기하는 시간
+    // 응답 대기 시간: 연결 된 뒤 요청을 보낸 뒤 재응답을 기다리는 시간
+
+    // 연결 타임아웃 설정: 5초
+    // 응답 대기 시간: 10초
     @Bean
     public WebClient.Builder webClientBuilder() {
         HttpClient httpClient = HttpClient.create()
@@ -36,4 +42,24 @@ public class WebClientConfig {
                 .maxInMemorySize(2 * 1024 * 1024)); // 최대 메모리 크기 설정 (예: 2MB)
     }
 
+    // 연결 타임 아웃, 응답 대기 시간 설정을 완화한 WebClient Builder
+    // 연결 타임 아웃: 10초
+    // 응답 대기 시간: 60초
+    @Bean
+    public WebClient.Builder longTimeoutWebClientBuilder() {
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10_000)
+                .responseTimeout(Duration.ofMinutes(60))
+                .doOnConnected(conn -> conn
+                        .addHandlerLast(new ReadTimeoutHandler(10_000, TimeUnit.MILLISECONDS))
+                        .addHandlerLast(new WriteTimeoutHandler(10_000, TimeUnit.MILLISECONDS))
+                );
+
+        return WebClient.builder()
+            .clientConnector(new ReactorClientHttpConnector(httpClient)) // 공통 HttpClient 설정 적용
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE) // 공통 헤더 설정
+            .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+            .codecs(clientCodecConfigurer -> clientCodecConfigurer.defaultCodecs()
+                .maxInMemorySize(2 * 1024 * 1024)); // 최대 메모리 크기 설정 (예: 2MB)
+    }
 }
