0844173a6d3cb65d01a648ccf6eb5ab6b22c7989
Gumraze-git <galaxydh4110@gmail.com>
2025-11-04 21:41:04 +0900

    refac(dashboard): 대시보드 서비스 메서드 요청 파라미터 수정 (#126)
    - 고객사 및 공급사 관련 서비스 호출 시 size 파라미터로 수정
    - 역할별 서비스 호출 메서드 선언 추가
      - 구매 요청, 출고/입고, 근태/휴가, 생산 관련 데이터 조회

--- src/main/java/org/ever/_4ever_be_gw/dashboard/service/impl/DashboardServiceImpl.java
+++ src/main/java/org/ever/_4ever_be_gw/dashboard/service/impl/DashboardServiceImpl.java
@@ -40,35 +40,58 @@ public class DashboardServiceImpl implements DashboardService {
 
         // 공급사 워크 플로우
         if (userType.equalsIgnoreCase("SUPPLIER")) {
-            // 주문서 목록 요청
-            ResponseEntity<ApiResponse<Object>> orderResponse = sdHttpService.getDashboardSupplierOrderList(userId, size);
+            // 발주서 목록 요청
+            ResponseEntity<ApiResponse<Object>> supplierQuotationResponse = sdHttpService.getDashboardSupplierOrderList(userId, size);
 
             // 매출 전표 요청(공급사 입장에서 매입 전표는 매출 전표)
             ResponseEntity<ApiResponse<Object>> supplierInvoiceResponse = fcmHttpService.getDashboardSupplierInvoiceList(userId, size);
         } else if (userType.equalsIgnoreCase("CUSTOMER")) {
             // 견적 목록 요청
-            ResponseEntity<ApiResponse<Object>> quotationResponse = sdHttpService.getDashboardCustomerQuotationList(userId, userType);
+            ResponseEntity<ApiResponse<Object>> quotationResponse = sdHttpService.getDashboardCustomerQuotationList(userId, size);
 
             // 매입 전표 요청(고객사 입장에서 매출 전표는 매입 전표)
-            ResponseEntity<ApiResponse<Object>> customerInvoiceResponse = fcmHttpService.getDashboardCustomerInvoiceList(userId, userType);
+            ResponseEntity<ApiResponse<Object>> customerInvoiceResponse = fcmHttpService.getDashboardCustomerInvoiceList(userId, size);
         } else {
             // 구매 관리
             if (userRole.startsWith("MM")) {
+                // 구매 요청서 목록 조회
+                ResponseEntity<ApiResponse<Object>> mmPurchaseRequestResponse = mmHttpService.getDashboardPurchaseRequestList(userId, size);
+
+                // 발주서 목록 조회
 
                 // 영업관리
             } else if (userRole.startsWith("SD")) {
 
+                // 고객사가 요청한 견적서 목록 조회
+
+                // 공급사에게 발행한 발주서 목록 조회
+
                 // 재무관리
             } else if (userRole.startsWith("FCM")) {
+
+                // 자사 매출 전표 목록 조회
+
+                // 자사 매입 전표 목록 조회
 
                 // 재고관리
             } else if (userRole.startsWith("IM")) {
 
+                // 입고 목록 조회
+
+                // 출고 목록 조회
+
                 // 인적자원관리
             } else if (userRole.startsWith("HRM")) {
 
+                // 직원 근태 목록 조회
+
+                // 직원 휴가 요청 목록 조회
+
                 // 생산관리
             } else {
+                // 영업관리에서 생산이 필요한 견적서가 재고 확인 후 전환된 견적서 목록 조회
+
+                // 현재 생산 중인 목록 조회
 
             }
         }
